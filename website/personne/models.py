66b43b112b4992cee59f8ea86ca33ac6b5cefa77
BlackCagouProd <blackcagou@outlook.fr>
2022-08-16 08:28:26 +0200

    add personne models

--- website/personne/models.py
+++ website/personne/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Personne(models.Model):
+
+    Nom_de_famille = models.CharField(max_length=200)
+    Prenom = models.CharField(max_length=200)
+    genre = models.CharField(max_length=1,
+                              choices=(('H', 'Homme'), ('F', 'Femme')),
+                              blank=False,
+                              default=None)
+    dateDeNaissance = models.ForeignKey('Event',
+                                        models.SET_NULL,
+                                        null=True,
+                                        blank=True,
+                                        related_name='+')
+    dateDeDeces = models.ForeignKey('Event',
+                                    models.SET_NULL,
+                                    null=True,
+                                    blank=True,
+                                    related_name='+')
+    deceder = models.BooleanField(default=True)
+    mere = models.ForeignKey('self',
+                               models.SET_NULL,
+                               blank=True,
+                               null=True,
+                               limit_choices_to={'genre': 'F'},
+                               related_name='enfants_de_la_mere')
+    pere = models.ForeignKey('self',
+                               models.SET_NULL,
+                               blank=True,
+                               null=True,
+                               limit_choices_to={'genre': 'H'},
+                               related_name='enfants_du_pere')
+
+    pub_date = models.DateTimeField('date de publication')
